c11a0bbd98ac89c881dc3072e49ad39ae37a012c
Oscar <nwanzeoscar@gmail.com>
2024-08-20 11:09:58 +0100

    prototype e-commerce frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,30 @@ module.exports = {
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: "15px",
+      },
+
+      colors: {
+        accent: "#FF8F9C",
+        blackish: "#1b1b1b",
+      },
+    },
+  },
+  plugins: [],
+};
+
+/*
+
+DEFAULT TAILWINDCSS 'extend' CONTENT BELOW:
+
+ extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
-  },
-  plugins: [],
-};
+
+*/
